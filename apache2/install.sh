e4e108c6fe5412044e5eeca90d62e4f6f0075f83
lucakuehne <luca.kuehne@gmail.com>
2024-04-05 16:04:55 +0200

    Update install.sh

--- apache2/install.sh
+++ apache2/install.sh
@@ -4,8 +4,9 @@ sudo apt update
 # Install Apache
 sudo apt install apache2
 
-# Check if Apache ist running
-sudo systemctl status apache2
-
 # TODO: Show link to test access to Apache2
-printf 'Check if access to apache works via \e]8;;http://IP/\e\\http://IP/\e]8;;\e\\\n'
+ip_address=$(hostname -I | awk '{print $1}')
+printf 'Check if access to apache works via \e]8;;http://$ip_address/\e\\http://$ip_address/\e]8;;\e\\\n'
+
+# Check if Apache ist running with the following command
+# sudo systemctl status apache2
