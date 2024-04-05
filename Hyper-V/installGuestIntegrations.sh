3f1faf5561a4480560840c5ac3600bb37aa9024c
lucakuehne <luca.kuehne@gmail.com>
2024-04-05 16:00:17 +0200

    Create installGuestIntegrations.sh

---
+++ Hyper-V/installGuestIntegrations.sh
@@ -0,0 +1,27 @@
+echo "Activate the guest integrations tools first in the VM-settings."
+read -p "Press enter to continue."
+
+# Add hv_modules to initramfs-tools modules.
+echo "hv_vmbus" >> /etc/initramfs-tools/modules
+echo "hv_storvsc" >> /etc/initramfs-tools/modules
+echo "hv_blkvsc" >> /etc/initramfs-tools/modules
+echo "hv_netvsc" >> /etc/initramfs-tools/modules
+
+# Update the apt packages
+sudo apt -y update
+
+# Upgrade the apt packages
+sudo apt -y upgrade
+
+# Install the Hyper-V integration packages for Ubuntu
+sudo apt -y install linux-virtual linux-cloud-tools-virtual linux-tools-virtual
+
+# Update initramfs
+sudo update-initramfs -u
+
+# Reboot
+read -p "Press enter to reboot."
+sudo reboot
+
+# Run the following command to ensure the LIS integration features are installed
+# lsmod | grep hv
