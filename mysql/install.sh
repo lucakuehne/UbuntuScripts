ceefba14abe8695a5e55ba673aeff1ca12a3cbc3
lucakuehne <luca.kuehne@gmail.com>
2025-01-29 12:57:51 +0100

    Create install.sh for mysql

---
+++ mysql/install.sh
@@ -0,0 +1,24 @@
+# Update the local package index
+sudo apt update
+
+# Install mysql
+sudo apt -y install mysql-server
+
+# Allow authentication of root in mysql by password. This is to prevent errors in the mysql_secure_installation script starting from 2022 on
+# Ask for password and save it to variable
+while true; do
+  read -p 'Please define a password for the root mysql login: ' mysql_root_password
+  read -p 'Please repeat the password for the root mysql login: ' mysql_root_password_confirm
+  if [[ "$mysql_root_password" == "$mysql_root_password_confirm" ]]; then
+    break
+  fi
+  echo 'The entered passwords do not match. Please try again.'
+done
+echo $mysql_root_password
+
+# Set password
+sudo mysql --execute="ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$mysql_root_password';"
+
+# Run the mysql_secure_installation script
+sudo mysql_secure_installation
+
